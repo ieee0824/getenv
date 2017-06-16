1cbc836614f8669a550c0acf29a40a706429d2bd
ieee0824 <ieee0824@ezweb.ne.jp>
2017-06-16 13:59:07 +0900

    duration取得する

--- duration.go
+++ duration.go
@@ -4,12 +4,32 @@ import (
 	"regexp"
 	"time"
 	"strconv"
+	"os"
 )
 
 var (
 	reAlp = regexp.MustCompile(`[a-zA-Z]+`)
 	reNum = regexp.MustCompile(`[0-9]+`)
 )
+
+func Duration(key string, def ...interface{}) time.Duration {
+	var d time.Duration
+	if len(def) != 0 {
+		if i, ok := def[0].(int); ok {
+			d = time.Duration(int64(i)) * time.Second
+		} else if dr, ok := def[0].(time.Duration); ok {
+			d = dr
+		} else if s, ok := def[0].(string); ok {
+			 d = parseDuration(s)
+		}
+	}
+
+	v := os.Getenv(key)
+	if v == "" {
+		return d
+	}
+	return parseDuration(v)
+}
 
 func trimArray(a []string) []string {
 	ret := []string{}
@@ -22,7 +42,7 @@ func trimArray(a []string) []string {
 	return ret
 }
 
-func Duration(s string) time.Duration {
+func parseDuration(s string) time.Duration {
 	alp := reAlp.Copy()
 	num := reNum.Copy()
 
